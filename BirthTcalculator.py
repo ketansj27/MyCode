736053336633d8cc5a3d9a730aa16d4196125b9e
Ketan Sanjay Jadhav <84120073+ketansj27@users.noreply.github.com>
2021-05-16 15:56:44 +0530

    Update BirthTcalculator.py

--- BirthTcalculator.py
+++ BirthTcalculator.py
@@ -1,17 +1,21 @@
-
 from datetime import datetime
-#pip3 install python-dateut
+# "pip3 install python-dateut", if not installed
 from dateutil import relativedelta
-import calendar
 
 
 s_date = input("Enter your Birth Date {dd/mm/yyyy} - ")
+
 print("\n")
-f_date = datetime.strptime(s_date, "%d/%m/%Y")
+try :
+    f_date = datetime.strptime(s_date, "%d/%m/%Y")
+except :
+    print(" please inter birth date in (dd/mm/yyyy) format.")
+    print("\n")
+    exit()
 #print(f_date)
 
-B_date = datetime.strftime(f_date,"%d %B, %Y")
-print("Birth Date : ",B_date)
+b_date = datetime.strftime(f_date,"%d %B, %Y")
+print("Birth Date : ",b_date)
 
 c_date = datetime.now()
 #print(c_date)
@@ -19,6 +23,7 @@ print("\n")
 #This will find the difference between the two dates
 
 difference = relativedelta.relativedelta(c_date, f_date)
+
 print("from your birth time count is ...\n")
 print(difference.years," years")
 print(difference.months," months")
