0d95b75b3d94dcbd66e962f75bff843b7f5b5d12
Ketan Sanjay Jadhav <84120073+ketansj27@users.noreply.github.com>
2021-06-08 22:33:58 +0530

    Update BirthTcalculator.py

--- BirthTcalculator.py
+++ BirthTcalculator.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-# "pip3 install python-dateut", if not installed
-from dateutil import relativedelta
-
+import time
+from dateutil import relativedelta          #"pip3 install python-dateutil", if not installed
 
 s_date = input("Enter your Birth Date {dd/mm/yyyy} - ")
 
@@ -10,25 +9,28 @@ try :
     f_date = datetime.strptime(s_date, "%d/%m/%Y")
 except :
     print(" please inter birth date in (dd/mm/yyyy) format.")
-    print("\n")
     exit()
-#print(f_date)
 
 b_date = datetime.strftime(f_date,"%d %B, %Y")
 print("Birth Date : ",b_date)
 
 c_date = datetime.now()
-#print(c_date)
-print("\n")
-#This will find the difference between the two dates
 
+#This will find the difference between the two dates
 difference = relativedelta.relativedelta(c_date, f_date)
 
 print("from your birth time count is ...\n")
-print(difference.years," years")
-print(difference.months," months")
-print(difference.days," days")
-print(difference.hours," hours")
-print(difference.minutes," minutes")
-print(difference.seconds," seconds")
-print(difference.microseconds," microseconds.")
+print("> ",difference.years,"years")
+time.sleep(0.3)
+print("> ",difference.months,"months")
+time.sleep(0.3)
+print("> ",difference.days,"days")
+time.sleep(0.3)
+print("> ",difference.hours,"hours")
+time.sleep(0.3)
+print("> ",difference.minutes,"minutes")
+time.sleep(0.3)
+print("> ",difference.seconds,"seconds")
+time.sleep(0.3)
+print("> ",difference.microseconds,"microseconds.")
+time.sleep(0.3)
