ccaa3529528b18ae3862eae574b3cb142f224801
Ketan Sanjay Jadhav <84120073+ketansj27@users.noreply.github.com>
2021-07-31 11:40:51 +0530

    Update repKey.py

--- repKey.py
+++ repKey.py
@@ -3,8 +3,18 @@ A company is transmitting data to another server. The data is in the form of num
 To secure the data during transmission, they plan to obtain a security key that will be 
 sent along with the data. The security key is identified as the count of the unique 
 repeating digits in the data.
+
+Input :The input consists of an integer data, representing the data to be transmitted.
+Output : Print an integer representing the security key for the given data. If no data is 
+        repeated it should display -1
+Example :
+Input: 578378923
+Output: 3
+Explanation
+The repeated digits in the data are 7, 8 and 3. So, the security key is 3.
 '''
-data = input("inter data : ")
+data = int(input("inter data : "))  # transmitting data is in 'int' format.
+data = str(data)                    # 'int' to 'str' for iteration purpose.
 list1 = list()
 
 counts = dict()
@@ -17,9 +27,13 @@ for digit in data:
 for key,value in counts.items() :
     #print(key,value)
     if value >= 2 :
-        list1.append(key)
+        list1.append(key)           # this list will append repeated int
 
-   
 #print(counts)
 #print(list1)
-print(len(list1))
+if (len(list1)) == 0:
+    key = -1
+else :
+    key = (len(list1))
+
+print(key)
