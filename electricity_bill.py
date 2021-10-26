e64debbc829dfdc73bfbe8e86b77702c9403acdd
Ketan Sanjay Jadhav <84120073+ketansj27@users.noreply.github.com>
2021-10-26 15:52:26 +0530

    Update electricity_bill.py

--- electricity_bill.py
+++ electricity_bill.py
@@ -1,5 +1,5 @@
 '''
-This code calculate energy bill based on tariff rate  of 'Mahavitaran' electricity
+This code calculate energy bill based on tariff eCharge  of 'Mahavitaran' electricity
 board.( valid => 1 Apl, 2021 - 31 March, 2022)
 '''
 class NotPositiveError(UserWarning): 
@@ -22,33 +22,28 @@ while True:
         break
     except NotPositiveError:
         print("Entered number was not positive, please try again.")
-        
-         
-print("\n")
-print("previous reading = ",previous_reading," unit")
-print("current reading =",current_reading," unit")
+           
+print("You intered your previous reading",previous_reading,"unit & current_reading as",current_reading,"unit.")
 print("\n")
 unit_consume = int(current_reading - previous_reading)
 print("Units consumed :",unit_consume,"unit \n")
 
-rate = float(0)
+eCharge = float(0)
 if unit_consume <= 100 :
-    rate = 3.44
-elif unit_consume <= 300 :          # this are the different rates as per unit consumed.
-    rate = 7.34
+    eCharge = 3.44 * unit_consume
+elif unit_consume <= 300 :                                               # this are the different eCharges as per unit consumed.
+    eCharge = (3.44*100)+(unit_consume-100)*7.34
 elif unit_consume <= 500 :
-    rate = 10.36
+    eCharge = (3.44*100)+(7.34*200)+(unit_consume-300)*10.36
 elif unit_consume <= 1000:
-    rate = 11.82
+    eCharge = (3.44*100)+(7.34*200)+(10.34*200)+(unit_consume-500)*11.82
 elif unit_consume > 1000 :
-    rate = 11.82
+    eCharge = (3.44*100)+(7.34*200)+(10.34*200)+(11.82*500)+(unit_consume-1000)*13.82
     
-print("According to unit consume, rate is ",rate,"Rs/unit \n")
-
-def computepay(rate,units):
-    fix_charge = float(102)          # As per the 1 Apl 2021 - 31 Mar 202 Fixed Charges & Wheeling Charges are taken.
+def computepay(eCharge,units):
+    fix_charge = float(102)                                 # As per the 1 Apl 2021 - 31 Mar 202 Fixed Charges & Wheeling Charges are taken.
     W_charge = float(1.38) 
-    return float(rate) * int(units) + float(fix_charge) + float(W_charge)*int(units)
+    return float(eCharge) + float(fix_charge) + float(W_charge)*int(units)
 
-To_pay = int(computepay(rate,unit_consume))
-print("Eelctricty bill is ",To_pay,"Rs")
+To_pay = int(computepay(eCharge,unit_consume))
+print("Eelctricty bill :",To_pay,"Rs")
